d1185d09030d1522fb00c3142cd01255205b27df
itsjustmithun <mitbusiness15@gmail.com>
2020-08-10 22:02:48 +0530

    Finishing touches

--- src/App.js
+++ src/App.js
@@ -105,16 +105,21 @@ function App() {
         
         <div className="app__stats">
           <InfoBox 
+          isRed
+            active={casesType === "cases"}
             onClick={e=> setCasesType('cases')}
             title="Coronavirus Cases" 
             cases={prettyPrintStat(countryInfo.todayCases)} 
             total={prettyPrintStat(countryInfo.cases)}/>
           <InfoBox 
+            active={casesType === "recovered"}
             onClick={e=> setCasesType('recovered')}
             title="Recovered" 
             cases={prettyPrintStat(countryInfo.todayRecovered)} 
             total={prettyPrintStat(countryInfo.recovered)}/>
-          <InfoBox 
+          <InfoBox
+          isRed 
+            active={casesType === "deaths"}
             onClick={e=> setCasesType('deaths')}
             title="Deaths" 
             cases={prettyPrintStat(countryInfo.todayDeaths)} 
@@ -132,8 +137,8 @@ function App() {
         <CardContent>
           <h3>Live Cases by Country</h3>
           <Table countries={tableData}/>
-            <h3>Worldwide New {casesType}</h3>
-            <LineGraph casesType={casesType} />
+            <h3 className="app__graphtitle">Worldwide New {casesType}</h3>
+            <LineGraph className="app__graph" casesType={casesType} />
         </CardContent>
       </Card>
     </div>
--- src/LineGraph.js
+++ src/LineGraph.js
@@ -62,7 +62,7 @@ const buildChartData = (data, casesType) => {
   return chartData;
 };
 
-function LineGraph({ casesType = "cases" }) {
+function LineGraph({ casesType = "cases", ...props }) {
   const [data, setData] = useState({});
 
   useEffect(() => {
@@ -82,7 +82,7 @@ function LineGraph({ casesType = "cases" }) {
   }, [casesType]);
 
   return (
-    <div>
+    <div className={props.className}>
       {data?.length > 0 && (
         <Line
           options={options}
