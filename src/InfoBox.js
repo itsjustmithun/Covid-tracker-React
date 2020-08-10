d7402b5e39b5649889cb3ec3621ed99c450cfcaf
itsjustmithun <mitbusiness15@gmail.com>
2020-08-10 20:47:29 +0530

    Modifying the infoboxes to fill the left container

--- src/InfoBox.js
+++ src/InfoBox.js
@@ -1,25 +1,30 @@
-import React from 'react'
-import { Card, CardCount, Typography, CardContent } from "@material-ui/core";
+import React from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
+import "./InfoBox.css";
 
-function InfoBox({ title, cases, total}) {
-    return (
-       <Card>
-           <CardContent>
-               {/* Title */}
-               <Typography className="infoBox__title" color="textSecondary">
-                   {title}
-               </Typography>
+function InfoBox({ title, cases, total, active, isRed, ...props }) {
+  console.log(title, active);
+  return (
+    <Card
+      onClick={props.onClick}
+      className={`infoBox ${active && "infoBox--selected"} ${
+        isRed && "infoBox--red"
+      }`}
+    >
+      <CardContent>
+        <Typography color="textSecondary" gutterBottom>
+          {title}
+        </Typography>
+        <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--green"}`}>
+          {cases}
+        </h2>
 
-               {/* Cases */}
-               <h2 className="infoBox__cases">{cases}</h2>
-
-               {/* TOTAL */}
-                <Typography className="infoBox__total" color="textSecondary">
-                    {total} Total
-                </Typography>
-           </CardContent>
-       </Card>
-    )
+        <Typography className="infoBox__total" color="textSecondary">
+          {total} Total
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 }
 
-export default InfoBox
+export default InfoBox;
