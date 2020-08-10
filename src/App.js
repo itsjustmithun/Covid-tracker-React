dfc4232052ade792b1e83dedb90fb60b3878ff86
itsjustmithun <mitbusiness15@gmail.com>
2020-08-10 16:20:55 +0530

    Retrieving country list from disease.sh site

--- src/App.js
+++ src/App.js
@@ -1,10 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { 
+  MenuItem,
+  FormControl,
+  Select,
+} from "@material-ui/core";
 import './App.css';
 
 function App() {
+  const [countries, setCountries] = useState([]); 
+  
+  //USEEFFECT = Runs a piece of code based on given condition
+
+  useEffect(() =>{
+    //This bascically runs code only once when the component loads
+    //Async = Send Request, wait for it
+    const getCountriesData = async () =>{
+      await fetch("https://disease.sh/v3/covid-19/countries")
+      .then((response) => response.json())
+      .then((data) =>{
+        const countries = data.map((country) => (
+          {
+            name: country.country,
+            value: country.countryInfo.iso2
+          }
+        ));
+
+        setCountries(countries);
+      });
+    };
+
+    getCountriesData();
+  }, []);
+
+
   return (
     <div className="app">
-      <h1>On a mission to have a working COVID 19 tracker - Mithun</h1>
+      <div className="app__header">
+      <h1>COVID TRACKER</h1>
+      <FormControl className="app__dropdown">
+        <Select
+          variant="outlined"
+          value="abc"
+        >
+          {/* Loop through all countries */}
+
+          {countries.map((country) =>(
+            <MenuItem value={country.value}>{country.name}</MenuItem>
+          ))}
+          {/* <MenuItem value="worldwide">WorldWide</MenuItem>
+          <MenuItem value="worldwide">test 2</MenuItem>
+          <MenuItem value="worldwide">test 3</MenuItem>
+          <MenuItem value="worldwide">test 4</MenuItem> */}
+        </Select>
+      </FormControl>
+      </div>
+      
 
       {/* header */}
       {/* Title + dropdown*/}
